14fcec80377720dffee31f6b5cde17b4bb52b56d
karming.wan <karming.wan@servicerocket.com>
2026-01-15 18:25:30 +0800

    fix: aws batch load job definition revision
    #2124

--- providers/aws/batch.go
+++ providers/aws/batch.go
@@ -73,7 +73,7 @@ func (g *BatchGenerator) loadJobDefinitions(batchClient *batch.Client) error {
 			return err
 		}
 		for _, jobDefinition := range page.JobDefinitions {
-			jobDefinitionName := StringValue(jobDefinition.JobDefinitionName) + ":" + fmt.Sprint(jobDefinition.Revision)
+			jobDefinitionName := StringValue(jobDefinition.JobDefinitionName) + ":" + fmt.Sprint(*jobDefinition.Revision)
 			g.Resources = append(g.Resources, terraformutils.NewResource(
 				jobDefinitionName,
 				jobDefinitionName,
